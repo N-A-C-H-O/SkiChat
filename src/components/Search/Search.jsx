7a22d77314dc0cbdca816419e365582f85536b2e
Ignacio Kruchowski <ignakruchowski@gmail.com>
2022-09-27 15:41:40 -0300

    Implementación userChats y chats

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,13 +1,14 @@
 import "./Search.css";
-import profileImg from "../Navbar/download.jpg";
-import { useState } from "react";
-import { collection, getDocs, query, where } from 'firebase/firestore';
+import { useContext, useState } from "react";
+import { collection, doc, getDoc, getDocs, query, serverTimestamp, setDoc, updateDoc, where } from 'firebase/firestore';
 import { db } from "../../utils/Firebase";
+import { AuthContext } from "../../context/AuthContext";
 
 export const Search = () => {
-
   const [username,setUsername] = useState("");
   const [user,setUser] = useState(null);
+
+  const {currentUser} = useContext(AuthContext)
   
   const handleSearch = async () => {
     const queryRef = query(collection(db, "users"), where("name", "==", username));
@@ -19,21 +20,60 @@ export const Search = () => {
     } catch (error) {
       console.log(error);
     }
+
+    setUsername("");
   }
 
   const handleKey = (e) => {
     e.code === "Enter" && handleSearch();
   }
 
+  const handleSelect = async () => {
+    const combinedId = currentUser.uid > user.uid ? 
+    currentUser.uid + user.uid :
+    user.uid + currentUser.uid;
+    try {
+      const res = await getDoc(doc(db,"chats",combinedId));
+      if(!res.exists()) {
+        await setDoc(doc(db,"chats",combinedId),{messages: []});
+
+        await updateDoc(doc(db,"userChats",currentUser.uid), {
+          [combinedId+".userInfo"]: {
+            uid: user.uid,
+            name: user.name,
+            photoURL: user.photoURL,
+          }, 
+          [combinedId+".date"]: serverTimestamp()
+        });
+
+        await updateDoc(doc(db,"userChats",user.uid), {
+          [combinedId+".userInfo"]: {
+            uid: currentUser.uid,
+            name: currentUser.name,
+            photoURL: currentUser.photoURL,
+          }, 
+          [combinedId+".date"]: serverTimestamp()
+        });
+
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    setUser(null);
+    setUsername("");
+  }
+
+
   return (
     <div className="search">
       <div className="search-form">
-        <input onChange={e => setUsername(e.target.value)} onKeyDown={handleKey} type="text" placeholder="Buscar usuario"/>
+        <input onChange={e => setUsername(e.target.value)} onKeyDown={handleKey} type="text" value={username} placeholder="Buscar usuario"/>
         <button onClick={handleSearch}>Buscar</button>
       </div>
       {
         user ? 
-        <div className="user-chat">
+        <div className="user-chat" onClick={handleSelect}>
           <img src={user.photoURL} alt="Imagen usuario"/>
           <div className="user-chat-info">
             <h4>{user.name}</h4>
