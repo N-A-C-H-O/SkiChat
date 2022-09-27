1cdd14329f8593496941f701d1cbe7233843cd23
Ignacio Kruchowski <ignakruchowski@gmail.com>
2022-09-27 20:24:21 -0300

    Implementación ChatContext

--- src/components/Chats/Chats.jsx
+++ src/components/Chats/Chats.jsx
@@ -1,14 +1,44 @@
 import "./Chats.css";
 import profileImg from "../Navbar/download.jpg";
+import { useContext, useEffect, useState } from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "../../utils/Firebase";
+import { AuthContext } from "../../context/AuthContext";
 
 export const Chats = () => {
+
+  const [chats,setChats] = useState([]);
+
+  const {currentUser} = useContext(AuthContext);
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data())
+      });  
+  
+      return () => {
+        unsub();
+      }
+    }
+
+    currentUser.uid && getChats();
+
+  },[currentUser.uid]);
+
+  console.log(chats)
+
   return (
-    <div className="user-chat">
-        <img src={profileImg} alt="Imagen usuario"/>
-        <div className="user-chat-info">
-            <h4>Nombre</h4>
-            <p>Mensaje</p>
+    Object.entries(chats)?.map((chat) => {
+      return(
+        <div className="user-chat" key={chat[0]}>
+          <img src={chat[1].userInfo.photoURL} alt="Imagen usuario"/>
+          <div className="user-chat-info">
+            <h4>{chat[1].userInfo.name}</h4>
+            <p>{chat[1].userInfo.lastMessage?.text}</p>
+          </div>
         </div>
-    </div>
+      );
+    })
   )
 }
