c48f92db24824bbff2b982186b1983c8e082094b
Ignacio Kruchowski <ignakruchowski@gmail.com>
2022-09-29 18:29:41 -0300

    Implementación notificaciones toastify

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -3,7 +3,7 @@ import Icon from "../../assets/images/logo_icon.png";
 
 export const Spinner = () => {
     return(
-        <div class="loader">
+        <div className="loader">
             <img src={Icon} alt="Loading" />
             <p>Cargando...</p>
         </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -6,13 +6,18 @@ import { auth, storage, db } from "../utils/Firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
 import Logo from "../assets/images/logo.png";
+import { useState } from "react";
+import { Spinner } from "../components/Spinner/Spinner";
 
 export const Register = () => {
+    const [loading,setLoading] = useState(false);
+    const [error,setError] = useState(null)
 
     const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setLoading(true);
         const inputName = e.target[0].value;
         const inputEmail = e.target[1].value;
         const inputPassword = e.target[2].value;
@@ -39,41 +44,58 @@ export const Register = () => {
                         navigate("/");
 
                     } catch(error) {
-                        console.log(error);
+                        console.log(error.code);
                     }
                 });
             });
 
         } catch (error) {
-            console.log(error);
+            switch (error.code) {
+                case "auth/invalid-email": 
+                    setError('Ingresa un email válido.');
+                    break;
+                case "auth/weak-password":
+                    setError('La contraseña debe tener al menos 6 caracteres');
+                    break;
+                case "auth/email-already-exists": 
+                    setError('El email ingresado ya existe');
+                    break;
+                default: setError('Se produjo un error');
+            };
         }
+        setLoading(false)
     }
 
-    return(
-        <div className="form-wrapper register">
-            <Link to="/">
-                <img src={Logo} alt="Logo" className="logo" />
-            </Link>
-            <form onSubmit={handleSubmit} autoComplete="off">
-                <div className="form-section">
-                    <label htmlFor="nombre">Nombre</label>
-                    <input type="text" placeholder="Ingrese su usuario" required/>
-                </div>
-                <div className="form-section">
-                    <label htmlFor="nombre">Email</label>
-                    <input type="email" placeholder="Ingrese su correo" required/>
-                </div>
-                <div className="form-section">
-                    <label htmlFor="nombre">Contraseña</label>
-                    <input type="password" placeholder="Ingrese su contraseña" required/>
-                </div>
-                <div className="form-section">
-                    <label htmlFor="userAvatar"><span className="avatar-icon"><BiImageAdd/></span>Añadir avatar</label>
-                    <input type="file" id="userAvatar"/>
-                </div>
-                <button type="submit">Enviar</button>
-                <Link to="/login" className="form-link">Ya estoy registrado <BsBoxArrowInRight/></Link>
-            </form>
-        </div>
-    )
+    if (loading) {
+        return <Spinner/>
+    } else {
+        return(
+            <div className="form-wrapper register">
+                <Link to="/">
+                    <img src={Logo} alt="Logo" className="logo" />
+                </Link>
+                <form onSubmit={handleSubmit} autoComplete="off">
+                    <div className="form-section">
+                        <label htmlFor="nombre">Nombre</label>
+                        <input type="text" placeholder="Ingrese su usuario" required maxLength={15}/>
+                    </div>
+                    <div className="form-section">
+                        <label htmlFor="nombre">Email</label>
+                        <input type="email" placeholder="Ingrese su correo" required maxLength={35}/>
+                    </div>
+                    <div className="form-section">
+                        <label htmlFor="nombre">Contraseña</label>
+                        <input type="password" placeholder="Ingrese su contraseña" required/>
+                    </div>
+                    <div className="form-section">
+                        <label htmlFor="userAvatar"><span className="avatar-icon"><BiImageAdd/></span>Añadir avatar</label>
+                        <input type="file" id="userAvatar"/>
+                    </div>
+                    <button type="submit">Enviar</button>
+                    <Link to="/login" className="form-link">Ya estoy registrado <BsBoxArrowInRight/></Link>
+                </form>
+                {error && <span>{error}</span>}
+            </div>
+        );
+    }
 }
